823119cdbe91c985e219ac25e02017996b7679c4
davidgfb <davidgracia99@gmail.com>
2021-06-21 19:56:22 +0200

    Create Rasterizador.py

---
+++ Rasterizador.py
@@ -0,0 +1,52 @@
+from numpy import array
+from pygame import init
+from pygame.draw import polygon
+from pygame.display import set_mode, flip
+    
+def ptoInterseccion(n, ptoPlano, d, ptoRayo):
+    anguloIncidencia = n @ d
+
+    if abs(anguloIncidencia) < 1e-6:
+        raise RuntimeError("error: el rayo es paralelo al plano")
+
+    direccionPlanoRayo = ptoRayo - ptoPlano
+    
+    return -n @ direccionPlanoRayo / anguloIncidencia * d + ptoPlano + direccionPlanoRayo
+
+def main():
+    ptosTri, z, ptosInterseccion = [[0,1,0],
+                                      [1,0,0],
+                                      [0,0,1]],\
+                                     array([0, 0, 1]), [] 
+    
+    n, ptoPlano, ptoRayo,  = z, -5 * z, 10 * z
+
+    for ptoTri in ptosTri:
+        rayDirection = ptoTri - ptoRayo
+        x,y,z = ptoInterseccion(n, ptoPlano, rayDirection, ptoRayo) # conversion a enteros
+
+        ptosInterseccion.append([x,y,z]) # conversion a array de enteros
+    
+    init()
+     
+    NEGRO, BLANCO, AZUL, VERDE, ROJO, pantalla, ptosPantalla, escala = (0, 0, 0), \
+                                       (255, 255, 255), (0, 0, 255),\
+                                       (0, 255, 0), (255, 0, 0),\
+                                        set_mode((300, 300)), [], 100  # tupla no mutable
+    
+    for pto in ptosInterseccion:
+        x,y,z=pto
+        ptosPantalla.append([escala * x, escala * y + escala])
+
+    print("ptosInterseccion =", ptosInterseccion, ", ptosPantalla =", ptosPantalla)
+    
+    polygon(pantalla, BLANCO, ptosPantalla, 0)
+
+    flip()
+
+main()
+
+# TODO:
+    # da la vuelta al eje Y
+    # no hay profundidad
+    # añade shader plano dep anguloIncidencia cos?
