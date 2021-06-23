46de148e72804a5b5c710b5ba0fa2157db9bba58
davidgfb <davidgracia99@gmail.com>
2021-06-23 20:11:05 +0200

    Update Rasterizador.py

--- Rasterizador.py
+++ Rasterizador.py
@@ -7,26 +7,24 @@ from time           import sleep
 from math           import sin, cos, radians
 from time           import time
 
-seMueveLuz = False #True
-estaDepurado = False
-
-CERO, Z, Y, X, UNITARIO, dFocal = array((0, 0, 0)), array((0, 0, 1)),\
+seMueveLuz, estaDepurado, CERO, Z, Y, X, UNITARIO, dFocal = True, False,\
+                                  array((0, 0, 0)), array((0, 0, 1)),\
                                   array((0, 1, 0)), array((1, 0, 0)),\
                                   array((1, 1, 1)), 1
 
-NEGRO, ROJO, VERDE, AZUL, BLANCO, PANTALLA, escala =\
-       CERO, 255 * X, 255 * Y, 255 * Z, 255 * UNITARIO, set_mode((300, 300)), 100
+NEGRO, ROJO, VERDE, AZUL, BLANCO, PANTALLA, escala, ptosInterseccion,\
+       distanciasPtos, ptosPantalla, ptoLuz, dLuz, ptosTri, n, ptoRayo, dTri_12,\
+       dTri_23, noTieneRelleno, t, fps, nFotogramas =\
+       CERO, 255 * X, 255 * Y, 255 * Z, 255 * UNITARIO, set_mode((300, 300)),\
+       100, [], [], [], 10 * Y, -Y, (X, Y, Z), Z, 3/2 * Z, X - Y, Z - X,\
+       0, time(), 60, 200
 
-ptosInterseccion, distanciasPtos, ptosPantalla = [], [], [] 
-
-ptoLuz, dLuz = 10 * Y, -Y # cenital hacia abajo
-ptosTri = (X, Y, Z)    
-n, ptoRayo = Z, 3/2 * Z 
+# cenital hacia abajo
 ptoPlano = ptoRayo - array((0, 0, dFocal))
 
 # saca la normal del plano del triangulo a partir de dos vectores entre puntos
-dTri_12, dTri_23 = X - Y, Z - X
 nTri = cross(dTri_12, dTri_23) # regla mano derecha # abs
+# negativo nada, 0 relleno, positivo wireframe
   
 def devuelveInterseccionPlanoRayo(n, d, ptoPlano, ptoRayo):
     '''n y d deben estar normalizados'''
@@ -58,6 +56,10 @@ def devuelveD_Sol():
 
     return d
 
+#tetraedro: (1,2,1), (2,0,0), (1,0,2), (0,0,0)
+
+#def gira():
+    
 def sombreaPlano(): # sombreador cara # la posicion por si sola no afecta a la luz. solo la dRotacion
     global dLuz
 
@@ -67,8 +69,6 @@ def sombreaPlano(): # sombreador cara # la posicion por si sola no afecta a la l
     dxLuz, dyLuz, dzLuz = dLuz
     xOrigenLinea = 150 # y largo linea
     ptoLinea = (xOrigenLinea * (dxLuz + 1), -xOrigenLinea * dyLuz)
-    aaline(PANTALLA, BLANCO, (xOrigenLinea, 0), ptoLinea, 1) 
-    circle(PANTALLA, BLANCO, ptoLinea, 5) 
 
     pEscalarN_Tri_Y_D_Luz = cosAnguloEntreVectores(nTri, dLuz) # pEscalarN_Tri_Y_D_Luz = abs(nTri @ dLuz / norm(nTri - dLuz)) # y = 1er ptoTri # -1 < pEscalarN_Tri_Y_D_Luz < 1 # cos 180º = -1 # backface culling
     # si positivo no esta mirando a camara (no render). si negativo esta mirando (render)
@@ -82,7 +82,7 @@ def sombreaPlano(): # sombreador cara # la posicion por si sola no afecta a la l
     cTri = VERDE
     colorTriAtenuado = cTri * pEscalarN_Tri_Y_D_Luz # gris
 
-    if estaDepurado: # afecta grav rendimiento
+    if estaDepurado: # reduce grav rendimiento
         print(40 * "#",
               "\n\nTri {nTri =", nTri, ", ptosTri =", ptosTri,
               
@@ -104,40 +104,43 @@ def sombreaPlano(): # sombreador cara # la posicion por si sola no afecta a la l
               "},\n\nSombreador {pEscalarN_Tri_Y_D_Luz =", pEscalarN_Tri_Y_D_Luz,
               ", cTri =", cTri, ", colorTriAtenuado =", colorTriAtenuado,
               "}\n")
-        
+
+    PANTALLA.fill(NEGRO) 
+
+    aaline(PANTALLA, BLANCO, (xOrigenLinea, 0), ptoLinea, 1) 
+    circle(PANTALLA, BLANCO, ptoLinea, 5)
     polygon(PANTALLA, colorTriAtenuado, ptosPantalla, noTieneRelleno)
 
+    update()
+
 # main
-for ptoTri in ptosTri:    #ptos o ptosEscena    
-    d = normaliza(ptoTri, ptoRayo)  
-    xI, yI, zI = devuelveInterseccionPlanoRayo(n, d, ptoPlano, ptoRayo) # conversion a enteros
 
-    distanciasPtos.append(norm(ptoTri - ptoRayo)) # ptoInterseccion - ptoRayo)) # zdepth desde pRayo o desde pInterseccion?
-    ptosInterseccion.append((xI, yI, zI)) # conversion a array de enteros
-        
-for ptoInterseccion in ptosInterseccion: 
-    xI, yI, zI = ptoInterseccion
-    ptosPantalla.append((escala * (xI + 1), -escala * (yI - 2)))
-    
-# aqui para sombreador vertices / aristas?
-
-noTieneRelleno = 0 # < 0 nada, = 0 relleno, > 0 wireframe
-
-t = time()
-fps = 60
+# TODO: capas (superficies blit)?
 
 init()
 
-nFotogramas = 200
 for nFotograma in range(nFotogramas):
-    PANTALLA.fill(NEGRO)
+    for ptoTri in ptosTri: # ptos o ptosEscena    
+        d = normaliza(ptoTri, ptoRayo)  
+        xI, yI, zI = devuelveInterseccionPlanoRayo(n, d, ptoPlano, ptoRayo) # conversion a enteros
+
+        distanciasPtos.append(norm(ptoTri - ptoRayo)) # ptoInterseccion - ptoRayo)) # zdepth desde pRayo o desde pInterseccion?
+        ptosInterseccion.append((xI, yI, zI)) # conversion a array de enteros
+            
+    for ptoInterseccion in ptosInterseccion: 
+        xI, yI, zI = ptoInterseccion
+        ptosPantalla.append((escala * (xI + 1), -escala * (yI - 2)))
+        
+    # aqui para sombreador vertices / aristas?
 
     #ptoLuz += array((0.1, 0, 0)) # para mover la camara en sentido positivo del eje X
 
     sombreaPlano()
-    update()
 
-    sleep(1 / fps) 
+    ptosInterseccion, distanciasPtos, ptosPantalla = [], [], [] # inicializa ptos: muy importante para no sobrecalcular (reduce grav rend llena memoria y parpadea)
+
+    if fps != 0:
+        sleep(1 / fps) 
   
 dt = time() - t
 print("\nhan pasado", round(dt, 2), "segundos para", nFotogramas, "fotogramas\n",\
