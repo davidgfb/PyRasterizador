a5445debd08e14a14f0bc166927c72c01a8e0be6
davidgfb <davidgracia99@gmail.com>
2021-06-25 14:58:14 +0200

    Update Giro.py

--- Giro.py
+++ Giro.py
@@ -9,6 +9,30 @@ PANTALLA, NEGRO, BLANCO, xOrigenLinea, nFotogramas, sentido, refresca,\
           anguloG = set_mode((300, 300)), array((0, 0, 0)),\
           255 * array((1, 1, 1)), 150, 400, 0, True, 0 #False # 0 antihorario, 1 horario
 
+'''
+def giraPlano(nombrePlano): # en un solo eje o plano (z = 0)
+    pto = (x,y,z)
+
+    if plano == "alfa": # xy 
+         z = 0 # pto = (x,y,0)
+    elif plano == "beta": # xz 
+         y = 0 # pto = (x,0,z)
+    elif plano == "gamma": # yz 
+         x = 0 # pto = (0,y,z)
+
+    return pto
+               
+def giraEje(nombreEje):
+    if eje == "x": # plano gamma yz (x=0)
+        pto = giraPlano(gamma)
+    elif eje == "y": # plano beta xz (y=0)
+        pto = giraPlano(beta)
+    elif eje == "z": # plano alfa xy (z=0)
+        pto = giraPlano(alfa)
+
+    return pto
+''' 
+
 def gira(): # en un solo eje o plano (z = 0)
     '''devuelve d normalizada'''
     global anguloG
@@ -21,14 +45,17 @@ def gira(): # en un solo eje o plano (z = 0)
     elif sentido == 1:
         d = (cos(anguloR), sin(anguloR), 0) # sentido horario
 
-    if anguloG < 360:
+    if anguloG < 359: # [0,359)
         anguloG += 1
+
+    else: # anguloG >= 359 [359, inf)
+        anguloG = 0
 
     return d
 
 init()
 
-for nFotograma in range(nFotogramas):
+while True: # for nFotograma in range(nFotogramas):
     d = gira()
     dx, dy, dz = d
 
